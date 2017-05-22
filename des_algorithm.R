afb1d023ef35e1fa7ab143df5afb9a2a679761f7
Nibiru-1337 <3psilion@gmail.com>
2017-05-22 13:46:53 +0200

    implemented expected norm of vector

--- des_algorithm.R
+++ des_algorithm.R
@@ -10,10 +10,34 @@
 ##' @return Best solution found.
 ##' 
 ##' @export
+##' 
+library('BBmisc')
+library('stringr')
+library('bbob')
+
+# https://math.stackexchange.com/questions/827826/average-norm-of-a-n-dimensional-vector-given-by-a-normal-distribution
+expected_norm_vec <- function(x){
+  N <- length(x)
+  numerator = sqrt(2)*gamma((N+1)/2)
+  denominator = gamma(N/2)
+  return(numerator/denominator)
+}
+
 des_algorithm <- function(par, fn, lower, upper, max_eval) {
+  
   dim <- length(upper)
   best_par <- NULL
   best_value <- Inf
+  
+  t <- 1
+  Delta <- 0
+  f <- 1/sqrt(2)
+  delta <- expected_norm_vec(rnorm(dim, 0, diag(dim)))
+  
+  # initialize first random population
+
+  
+  
   for (i in 1:max_eval) {
     par <- runif(dim, min=lower, max=upper)
     value <- fn(par)
