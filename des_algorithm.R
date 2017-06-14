f6e16927765a5d2422f9ff2f3397f2a94fbc53dd
Kacper Trojanowski <trojanowski.kacper@gmail.com>
2017-06-14 23:18:58 +0200

    Broke down code into functions

--- des_algorithm.R
+++ des_algorithm.R
@@ -1,5 +1,5 @@
-##' Implementation of Differential Evolution Strategy (D. Jagodzinski, J. Arabas)
-##' 
+##' Implementation of Differential Evolution Strategy (K. Breński, K. Trojanowski)
+##' Authors of algorithm: D. Jagodzinski, J. Arabas
 ##'
 ##' @param par Initial parameter combination .
 ##' @param fn Function to minimize.
@@ -38,23 +38,8 @@ des_algorithm <- function(par, fn, lower, upper, max_eval, tunable_parameters = 
     return(array(c(new_population,history),dim=c(lambda,dim,H)))
   }
   
-  dim <- length(upper)
-  best_par <- NULL
-  best_value <- Inf
-  evals <- 0
-  
-  # initialize non-tunable parameters
-  t <- 1
-  Delta <- 0
-  delta <- expected_norm_vec(rnorm(dim, 0, 1))
-  
-  # initialize tunable parameters (with default values)
-    f <- 1/sqrt(2)
-    lambda <- 10
-    mu <- 5
-    c <- 4/(dim+4)
-    H <- 6+3*sqrt(dim)
-    epsilon <- 1e-8/expected_norm_vec(rnorm(dim,0,1))
+  read_custom_parameters <- function(){
+
     
     if(!is.null(tunable_parameters)){
       if(!is.null(tunable_parameters[['f']])) {
@@ -76,7 +61,37 @@ des_algorithm <- function(par, fn, lower, upper, max_eval, tunable_parameters = 
         epsilon <- tunable_parameters[['epsilon']]
       } 
     }
-    
+  }
+  
+  correction_vector <- function() {
+    return(f*(P.history[j,,h] - P.history[k,,h]) + Delta*delta*rnorm(1))
+  }
+  
+  ordered_population <- function() {
+    vals_and_agents <- cbind(apply(P,1,q), P)
+    P_ordered <- vals_and_agents[order(vals_and_agents[,1]),]
+    return(P_ordered)
+  }
+  
+  dim <- length(upper)
+  # initialize non-tunable parameters
+  t <- 1
+  Delta <- 0
+  delta <- expected_norm_vec(rnorm(dim, 0, 1))
+  
+  # initialize tunable parameters (with default values)
+  f <- 1/sqrt(2)
+  lambda <- 10
+  mu <- 5
+  c <- 4/(dim+4)
+  H <- 6+3*sqrt(dim)
+  epsilon <- 1e-8/expected_norm_vec(rnorm(dim,0,1))
+  read_custom_parameters()
+  
+  best_par <- NULL
+  best_value <- Inf
+  evals <- 0
+  
   # initialize population history
   P.history <- array(0,dim=c(lambda, dim, H))
   
@@ -87,10 +102,9 @@ des_algorithm <- function(par, fn, lower, upper, max_eval, tunable_parameters = 
   repeat{
     P_midpoint <- colSums(P)/lambda
     P_midpoint_val <- q(P_midpoint)
-    vals_and_agents <- cbind(apply(P,1,q), P)
-    evals <- evals + lambda + 1
-    P_ordered <- vals_and_agents[order(vals_and_agents[,1]),]
+    P_ordered <- ordered_population()
     mu_midpoint <- colSums(head(P_ordered[,-1], mu)) / mu
+    evals <- evals + lambda + 1
     
     if(P_ordered[1,1] < best_value) {
       best_par <- P_ordered[1,-1]
@@ -103,10 +117,9 @@ des_algorithm <- function(par, fn, lower, upper, max_eval, tunable_parameters = 
       # it's 0,H-1 in the paper but we use indexing from 1 to H for historical populations
       # h = 1 is for current population (t), H is for earliest stored (t-H+1) 
       h <- runif(1,1,min(H,t)) 
-      
       j <- runif(1,1,mu)
       k <- runif(1,1,mu)
-      d <- f*(P.history[j,,h] - P.history[k,,h]) + Delta*delta*rnorm(1)
+      d <- correction_vector()
       P[i,] <- mu_midpoint + d + epsilon*rnorm(dim, 0, 1)
     }
     t <- t + 1
@@ -116,7 +129,6 @@ des_algorithm <- function(par, fn, lower, upper, max_eval, tunable_parameters = 
   }
   list(par=best_par, value=best_value)
 }
-
 # https://math.stackexchange.com/questions/827826/average-norm-of-a-n-dimensional-vector-given-by-a-normal-distribution
 expected_norm_vec <- function(x){
   N <- length(x)
@@ -124,4 +136,3 @@ expected_norm_vec <- function(x){
   denominator = gamma(N/2)
   return(numerator/denominator)
 }
-
