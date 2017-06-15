29e0513627cdae66ae55ee8ea030bb0583ae601d
Kacper Trojanowski <trojanowski.kacper@gmail.com>
2017-06-15 20:41:50 +0200

    added charts and short description

--- run_experiment.R
+++ run_experiment.R
@@ -1,23 +1,15 @@
 library('BBmisc')
 library('stringr')
 library('bbob')
-if(!exists("des_algorithm", mode="function")) source("des_algorithm.R")
-
+library('DEoptim')
 des_optimizer <- function(par, fun, lower, upper, max_eval) {
-  n<-length(lower)
-  tunable_params = list(
-    f=1/sqrt(2),
-    lambda=4*n,
-    mu=2*n,
-    c=4/(n+4),
-    H=6+3*sqrt(n),
-    epsilon=1e-8/expected_norm_vec(rnorm(n,0,1))
-  )
-  des_algorithm(par,fun,lower,upper,max_eval,tunable_params)
+  population.number <- length(lower)*10 # default population number of DEoptim
+  ctrl <- DEoptim.control(itermax = max_eval/population.number,trace=FALSE)
+  DEoptim(fun,lower,upper,ctrl)
 }
 
 run_benchmark <- function() {
-  alg_id <- sprintf("default_des")
+  alg_id <- sprintf("DEoptim")
   dir <- sprintf("data/%s_%s",alg_id,format(Sys.time(), "%d%h%H%M"))
   bbo_benchmark(des_optimizer, alg_id, dir,budget=10000, dimensions = c(2,3,5,10,20,40))
 }
